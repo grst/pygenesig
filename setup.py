93bd8019221b5774cc59b33b4975c6c12f5f5038
Gregor Sturm <mail@gregor-sturm.de>
2021-06-06 16:55:28 +0200

    Fix package metadata

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="pygenesig",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     description="Create and Validate Gene Signatures",
     author="Gregor Sturm",
-    author_email="gregor.sturm@cs.tum.edu",
-    url="https://github.com/grst/chunksub",  # use the URL to the github repo
+    author_email="mail@gregor-sturm.de",
+    url="https://github.com/grst/pygenesig",  # use the URL to the github repo
     keywords=["bioinformatics", "gene expression", "signatures"],
     license="MIT",
     install_requires=[
-        "jinja2",
         "pyyaml",
-        "docopt",
         "seaborn",
         "numpy",
         "pandas",
@@ -29,4 +34,6 @@ setup(
     ),
     classifiers=[],
     include_package_data=True,
+    long_description_content_type="text/x-rst",
+    long_description=long_description,
 )
