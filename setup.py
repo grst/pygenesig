cde68fcd0e321daa4e32f45d3eb08abe5278206c
Gregor Sturm <mail@gregor-sturm.de>
2021-07-31 19:59:56 +0200

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="pygenesig",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     description="Create and Validate Gene Signatures",
     author="Gregor Sturm",
